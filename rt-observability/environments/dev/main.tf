8b43f21a3e2bddff989ca4908322e122b9725063
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-06-28 23:02:52 +0100

    ecgo

--- rt-observability/environments/dev/main.tf
+++ rt-observability/environments/dev/main.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
 }
 
 module "prometheus" {
-  source = "./"  # Reference the current directory
+  source = "git::https://github.com/c00perdon/Production.git//rt-observability/modules/prometheus"
 
   prometheus_image             = var.prometheus_image
   prometheus_replicas          = var.prometheus_replicas
