5a55c1e9de6146becf3a9c119e7a3d3b0543c19e
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-06-28 15:20:33 +0100

    new

--- rt-observability/environments/dev/main.tf
+++ rt-observability/environments/dev/main.tf
@@ -18,47 +18,5 @@ module "prometheus" {
   prometheus_replicas          = var.prometheus_replicas
   prometheus_scrape_interval   = var.prometheus_scrape_interval
   prometheus_storage_retention = var.prometheus_storage_retention
-  prometheus_config_data       = merge(
-    {
-      global = {
-        scrape_interval = var.prometheus_scrape_interval
-      }
-      scrape_configs = [
-        {
-          job_name     = "prometheus"
-          static_configs = [
-            {
-              targets = ["localhost:9090"]
-            }
-          ]
-        }
-      ]
-    },
-    var.prometheus_config_data
-  )
-}
-
-variable "prometheus_image" {
-  description = "Docker image for Prometheus"
-  type        = string
-}
-
-variable "prometheus_replicas" {
-  description = "Number of replicas for Prometheus deployment"
-  type        = number
-}
-
-variable "prometheus_scrape_interval" {
-  description = "Scrape interval for Prometheus"
-  type        = string
-}
-
-variable "prometheus_storage_retention" {
-  description = "Time duration for which data should be retained in Prometheus"
-  type        = string
-}
-
-variable "prometheus_config_data" {
-  description = "Data for prometheus.yml configuration"
-  type        = map
+  prometheus_config_data       = var.prometheus_config_data
 }
--- rt-observability/environments/dev/variables.tfvars
+++ rt-observability/environments/dev/variables.tfvars
@@ -1,9 +1,20 @@
-# environments/dev/variables.tfvars
-
-# Example variables for dev environment
-prometheus_image = "prom/prometheus:v2.30.3"
-prometheus_replicas = 1
-prometheus_scrape_interval = "15s"
+prometheus_image             = "prom/prometheus:v2.30.3"
+prometheus_replicas          = 1
+prometheus_scrape_interval   = "15s"
 prometheus_storage_retention = "12h"
 
-# Add more variables as needed
+prometheus_config_data = {
+  global = {
+    scrape_interval = "15s"
+  }
+  scrape_configs = [
+    {
+      job_name     = "prometheus"
+      static_configs = [
+        {
+          targets = ["localhost:9090"]
+        }
+      ]
+    }
+  ]
+}
