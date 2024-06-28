6e91186d0cc0b7f8df15ec309979f1e08f06d9a3
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-06-28 17:51:04 +0100

    fgfdd

--- rt-observability/modules/prometheus/outputs.tf
+++ rt-observability/modules/prometheus/outputs.tf
@@ -1,23 +1,23 @@
 output "prometheus_service_name" {
-  value = kubernetes_service.prometheus.metadata.name
+  value = kubernetes_service.prometheus[0].metadata.name
 }
 
 output "prometheus_service_port" {
-  value = kubernetes_service.prometheus.spec.ports[0].port
+  value = kubernetes_service.prometheus[0].spec[0].ports[0].port
 }
 
 output "prometheus_service_cluster_ip" {
-  value = kubernetes_service.prometheus.spec.cluster_ip
+  value = kubernetes_service.prometheus[0].spec[0].cluster_ip
 }
 
 output "prometheus_deployment_name" {
-  value = kubernetes_deployment.prometheus.metadata.name
+  value = kubernetes_deployment.prometheus[0].metadata.name
 }
 
 output "prometheus_image" {
-  value = kubernetes_deployment.prometheus.spec.template.spec.container[0].image
+  value = kubernetes_deployment.prometheus[0].spec[0].template[0].spec[0].container[0].image
 }
 
 output "prometheus_config_map_name" {
-  value = kubernetes_config_map.prometheus_config.metadata.name
+  value = kubernetes_config_map.prometheus_config[0].metadata.name
 }
