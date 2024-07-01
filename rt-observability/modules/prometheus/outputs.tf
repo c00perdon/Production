59e4ac55b1666b817ddddc882616a57eb0ebf72a
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-07-01 15:15:00 +0100

    bnbfgf

--- rt-observability/modules/prometheus/outputs.tf
+++ rt-observability/modules/prometheus/outputs.tf
@@ -1,25 +1,25 @@
-# prometheus/outputs.tf1
+# outputs.tf in Grafana module
 
-output "prometheus_service_name" {
-  value = kubernetes_service.prometheus.metadata[0].name
+output "grafana_endpoint" {
+  value = kubernetes_service.grafana.spec[0].cluster_ip
 }
 
-output "prometheus_service_port" {
-  value = kubernetes_service.prometheus.spec[0].port[0].port
+output "grafana_service_name" {
+  value = kubernetes_service.grafana.metadata[0].name
 }
 
-output "prometheus_service_cluster_ip" {
-  value = kubernetes_service.prometheus.spec[0].cluster_ip
+output "grafana_service_port" {
+  value = kubernetes_service.grafana.spec[0].ports[0].port
 }
 
-output "prometheus_deployment_name" {
-  value = kubernetes_deployment.prometheus.metadata[0].name
+output "grafana_deployment_name" {
+  value = kubernetes_deployment.grafana.metadata[0].name
 }
 
-output "prometheus_image" {
-  value = kubernetes_deployment.prometheus.spec[0].template[0].spec[0].container[0].image
+output "grafana_image" {
+  value = kubernetes_deployment.grafana.spec[0].template[0].spec[0].containers[0].image
 }
 
-output "prometheus_config_map_name" {
-  value = kubernetes_config_map.prometheus_config.metadata[0].name
+output "grafana_config_map_name" {
+  value = kubernetes_config_map.grafana_config.metadata[0].name
 }
