3134fdae81ddc5505ecb5f80e0a9f8824ff4eb59
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-07-01 12:32:22 +0100

    ssave

--- rt-observability/environments/dev/main.tf
+++ rt-observability/environments/dev/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.11.1"  # Adjust version as needed
+      version = ">= 1.11.1"  
     }
   }
 }
